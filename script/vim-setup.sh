c432c802f80a6cf5091b051a51412c100c8bdda1
n4kz <nfokz@yandex.ru>
2012-11-12 22:16:14 +0000

    Shell script to downlod vim files

---
+++ script/vim-setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Script to download vim files from github and push'em into .dotfiles/vim directory
+
+cd $(dirname $0);
+sources=$(cat sources.list)
+
+cd ..
+
+if ! [ -d vim ]; then
+	mkdir vim
+fi
+
+cd vim
+
+for url in $sources; do
+	folder=$(echo $url | sed -e 's/.*\///')-master
+
+	wget $url/archive/master.zip
+
+	unzip -o master.zip
+	cp -rv $folder/* ./
+	rm -rfv $folder master.zip README* Make* *.md
+done
