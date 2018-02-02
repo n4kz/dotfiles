2e0e25e5387008ab37f74d650a918c5fcce98f3f
n4kz <n4kz@n4kz.com>
2018-02-02 17:37:46 +0300

    Added moex rates to bitbar

---
+++ bitbar/.bitbar/rates.60s.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+source $HOME/.profile
+
+DATA=$(http https://www.banki.ru/products/currency/ajax/informer/data/ | jq '.moex[] | .value')
+RATE=( $DATA )
+
+printf "%.2f %.2f" ${RATE[0]} ${RATE[1]}
